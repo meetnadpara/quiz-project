7c1da54273fce73bfd0c5711bcf7b5f188da29e0
Parth Faladu <parthfaladu@Parths-MacBook-Air.local>
2021-07-16 09:58:43 +0530

    with tailwind basic setup

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "3rem",
+        "2xl": "3rem",
+      },
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
